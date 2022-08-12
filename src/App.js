30c49ac5f41588361d82a66c0c6233be4747c55d
Fuego <hoanganhjalai12@gmail.com>
2022-08-12 22:29:36 +0700

    uncompleted-v.unknown

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import TypingV2 from "./options/Typing_v2";
 function App() {
   return (
     <>
-      {/* <TypingV1 /> */}
-      <TypingV2 />
+      <TypingV1 />
+      {/* <TypingV2 /> */}
     </>
   )
 }
--- src/options/Typing_v1.js
+++ src/options/Typing_v1.js
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from "react";
 import ContextWords from "../components/ContextWords";
 import PopUpContent from "../components/PopUpContent";
+import { useState, useEffect, useRef } from "react";
 
 export default function TypingV1(){
     const paragraphCollection = ["ê tình yêu là gì đây là một câu hỏi vừa khó vừa dễ bởi hầu hết ai trong chúng ta khi trưởng thành cũng đều ít nhất một lần yêu nhưng lại chẳng ai có thể định nghĩa được rõ ràng như thế nào là yêu thế nào là tình yêu chân chính có người nói rằng tình yêu là khi có người ở bên cạnh yêu thương quan tâm và chia sẻ mọi thứ một số người lại cho rằng tình yêu là sự đồng cảm rung động giữa hai người có sự hấp dẫn đích thực về giới tính có lẽ chỉ khi yêu mỗi người mới cảm nhận được một cách đúng nghĩa và đầy đủ về tình yêu tiền tệ là một phạm trù lịch sử nó là sản phẩm tự phát của nền kinh tế hàng hóa sản phẩm của sự phát triển các hình thái giá trị đồng thời cũng là sản phẩm của sự phát triển mâu thuẫn giữa lao động và phân công lao động xã hội trong sản xuất hàng hóa sự ra đời và phát triển của tiền tệ gắn liền với sự ra đời và phát triển của sản xuất và trao đổi hàng hóa theo mac tiền tệ là một thứ hàng hoá đặc biệt được tách ra khỏi thế giới hàng hoá dùng để đo lường và biểu hiện giá trị của tất cả các loại hàng hoá khác nó trực tiếp thể hiện lao động xã hội và biểu hiện quan hệ sản xuất giữa những người sản xuất hàng hóa biến đổi khí hậu là thuật ngữ được dùng để chỉ sự thay đổi của khí hậu do tác động chủ yếu của con người làm thay đổi các thành phần của khí quyển trái đất sự thay đổi này kết hợp với các yếu tố biến động tự nhiên của tự nhiên dẫn tới các biến đổi của khí hậu qua các thời kỳ nói một cách dễ hiểu biến đổi khí hậu chính là sự thay đổi của hệ thống khí hậu từ sinh quyển khí quyển thủy quyền tới thạch quyển trong hiện tại và tương lai"]
@@ -122,10 +122,24 @@ export default function TypingV1(){
     function func(a, b) {  
       return 0.5 - Math.random();
     }  
-  
+    const colors = [
+      "linear-gradient(to right, #571845 0%,#571845 20%,#900c3e 20%,#900c3e 40%,#c70039 40%,#c70039 60%,#ff5733 60%,#ff5733 80%,#ffc300 80%,#ffc300 100%)",
+      "linear-gradient(to right, #00ffff 0%,#00ffff 20%,#33d6ff 20%,#33d6ff 40%,#66adff 40%,#66adff 60%,#cc5cff 60%,#cc5cff 80%,#ff33ff 80%,#ff33ff 100%)",
+      "linear-gradient(to right, #0077b6 0%,#0077b6 20%,#0096c7 20%,#0096c7 40%,#00b4d8 40%,#00b4d8 60%,#48cae4 60%,#48cae4 80%,#90e0ef 80%,#90e0ef 100%)",
+      "linear-gradient(to right, #cdb4db 0%,#cdb4db 20%,#ffc8dd 20%,#ffc8dd 40%,#ffafcc 40%,#ffafcc 60%,#bde0fe 60%,#bde0fe 80%,#a2d2ff 80%,#a2d2ff 100%)",
+      "linear-gradient(to right, #f72585 0%,#f72585 20%,#b5179e 20%,#b5179e 40%,#7209b7 40%,#7209b7 60%,#560bad 60%,#560bad 80%,#480ca8 80%,#480ca8 100%)",
+      "linear-gradient(to right, #ffe5ec 0%,#ffe5ec 20%,#ffc2d1 20%,#ffc2d1 40%,#ffb3c6 40%,#ffb3c6 60%,#ff8fab 60%,#ff8fab 80%,#fb6f92 80%,#fb6f92 100%)",
+      "linear-gradient(to right, #5a189a 0%,#5a189a 20%,#7b2cbf 20%,#7b2cbf 40%,#9d4edd 40%,#9d4edd 60%,#c77dff 60%,#c77dff 80%,#e0aaff 80%,#e0aaff 100%)",
+      "linear-gradient(to right, #ff7b00 0%,#ff7b00 20%,#ff8800 20%,#ff8800 40%,#ff9500 40%,#ff9500 60%,#ffa200 60%,#ffa200 80%,#ffaa00 80%,#ffaa00 100%)",
+      "linear-gradient(to right, #7bdff2 0%,#7bdff2 20%,#b2f7ef 20%,#b2f7ef 40%,#eff7f6 40%,#eff7f6 60%,#f7d6e0 60%,#f7d6e0 80%,#f2b5d4 80%,#f2b5d4 100%)",
+    ]
+    const [color, setColor] = useState(3)
   
     function randomParagraph(){
-      setWords(wordsValue())
+      if(color<colors.length-1){
+        setColor(prev => prev+1)
+      }
+      else setColor(0)
     }
   
   
@@ -153,7 +167,7 @@ export default function TypingV1(){
     }
   
     return (
-      <div className="main">
+      <div className="main" style={{backgroundImage: colors[color]}}>
         <PopUpContent 
           words = {words} 
           letter = {letter}
